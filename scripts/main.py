f1159172617017725c5f06855ad02ab382744f7d
xie river <chuanxie@amazon.com>
2023-04-17 10:29:15 +0000

    change default concept dir value to placeholder

--- scripts/main.py
+++ scripts/main.py
@@ -1029,7 +1029,7 @@ def on_ui_tabs():
 def build_concept_panel():
     with gr.Column():
         gr.HTML(value="Directories")
-        instance_data_dir = gr.Textbox(label='Dataset Directory', value='/opt/ml/input/data/concepts', placeholder="Path to directory with input images")
+        instance_data_dir = gr.Textbox(label='Dataset Directory', placeholder="/opt/ml/input/data/concepts")
         class_data_dir = gr.Textbox(label='Classification Dataset Directory', placeholder="(Optional) Path to directory with classification/regularization images")
     with gr.Column():
         gr.HTML(value="Filewords")
